846c5343f6b3583ef7f17ca1aa8c1ccac8abc2d5
Kate <krushtie33@gmail.com>
2024-02-27 23:01:15 +0000

    Changed naming from total_rows to number_rows_of_stars to make it clearer pattern.py

--- pattern.py
+++ pattern.py
@@ -1,13 +1,13 @@
-total_rows = 4 #total number of rows because we start on 0
+number_rows_of_stars = 4 #total number of rows because we start on 0
 start = 0 #starting row number
-end = (2 * total_rows) #last row number
+end = (2 * number_rows_of_stars) #last row number
 star_icon = "*"
 
 #Create a for loop with if and else statements:
 
 
 for row_number in range(start, end):  #Each row has a consequentive number from start to end for all the total rows
-    if row_number <= total_rows-1: #the number of stars is the same as the row number, if the row number is 5 or less
+    if row_number <= number_rows_of_stars-1: #the number of stars is the same as the row number, if the row number is 5 or less
         print(star_icon)
         star_icon = star_icon + "*"
     else:
