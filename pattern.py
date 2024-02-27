cdcb439d23e497e2c44fc4955871683f4a7ebd88
Kate <krushtie33@gmail.com>
2024-02-27 23:41:32 +0000

    Changed * to @ for a cooler look pattern.py

--- pattern.py
+++ pattern.py
@@ -1,7 +1,7 @@
 number_rows_of_stars = 4 #total number of rows because we start on 0
 start = 0 #starting row number
 end = (2 * number_rows_of_stars) #last row number
-star_icon = "*"
+star_icon = "@"
 
 #Create a for loop with if and else statements:
 
@@ -9,8 +9,8 @@ star_icon = "*"
 for row_number in range(start, end):  #Each row has a consequentive number from start to end for all the total rows
     if row_number <= number_rows_of_stars-1: #the number of stars is the same as the row number, if the row number is 5 or less
         print(star_icon)
-        star_icon = star_icon + "*"
+        star_icon = star_icon + "@"
     else:
         print(star_icon)
         star_icon = star_icon[:-1] #Otherwise, print the reverse order
-print("*")
+print("@")
